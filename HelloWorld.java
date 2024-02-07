e5fc01d00eae89c9f84320410c2adc468b2239cb
Heetika22 <112322861+Heetika22@users.noreply.github.com>
2024-02-07 09:46:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
-    public static void main{
-        System.out.println("Hello Heetika")
+    public static void main(String args[]){
+        System.out.println("Hello Heetika");
     }
 }
